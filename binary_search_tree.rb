2eed99a1d1035b34d8bf1a3e494cf7d233d10ba0
Kropyls <Kropyls@gmail.com>
2022-08-16 15:26:31 -0700

    added insert method. Touched up build_tree method.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -7,6 +7,8 @@ class Node
 
   def initialize(root)
     @data = root
+    @left = nil
+    @right = nil
   end
 
   def <=>(other)
@@ -20,7 +22,6 @@ class Tree
 
   def initialize(arr)
     arr.uniq!.sort!
-    # build tree here
     @root = build_tree(arr)
   end
 
@@ -29,12 +30,15 @@ class Tree
     return nil if arr.empty?
 
     mid = (arr.count / 2.0).ceil - 1
-    left_arr = arr[0...mid]
-    right_arr = arr[(mid + 1)...arr.count]
     root = Node.new(arr[mid])
-    root.left = build_tree(left_arr)
-    root.right = build_tree(right_arr)
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[(mid + 1)...arr.count])
     root
+  end
+
+  def insert(value)
+    leaf = Node.new(value)
+    @root = priv_insert(leaf)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -42,9 +46,25 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  private
+
+  def priv_insert(leaf_node, node = @root)
+    return leaf_node if node.nil?
+
+    case leaf_node <=> node
+    when -1
+      node.left = priv_insert(leaf_node, node.left)
+    when 0
+      puts 'Can not insert! Value already exists in tree'
+    when 1
+      node.right = priv_insert(leaf_node, node.right)
+    end
+    node
+  end
 end
 
 arr = [1, 2, 7, 4, 23, 8, 9, 4, 55, 3, 5, 7, 9, 67, 6345, 324]
 x = Tree.new(arr)
+x.insert(55)
 x.pretty_print
-
